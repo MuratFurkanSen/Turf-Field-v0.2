26b8035ca746b4b488a383735dcc1716cd104912
FranzKAFKAS <franzkafkas36@gmail.com>
2025-05-09 18:30:25 +0300

    User information Page Created

--- Turf_Field/settings.py
+++ Turf_Field/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k0+8dkw*f3=r_e&b$@$i(8wwjx5q&b24=l@$a8n^i=zmdi*^w+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['188.119.38.49']
+ALLOWED_HOSTS = ['188.119.38.49', 'localhost']
 
 # Application definition
 
